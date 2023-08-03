58290811ba8fe8018956f5b63ccdb0acb974887a
Omer-5 <OmerHazan5@gmail.com>
2023-08-03 19:09:55 +0300

    add Azure database connection

---
+++ src/Store/AzureSqlConnection.java
@@ -0,0 +1,40 @@
+package Store;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+// import Secrets;
+
+public class AzureSqlConnection {
+    // Azure SQL Database connection details
+    PropertiesReader reader = new PropertiesReader("src/store/Secrets.properties");
+    private final String SERVER_NAME = reader.getProperty("SERVER_NAME");
+    private final String DATABASE_NAME = reader.getProperty("DATABASE_NAME");
+    private final String USERNAME = reader.getProperty("USERNAME");
+    private final String PASSWORD = reader.getProperty("PASSWORD");
+    // private static final String CONNECTION_URL = "jdbc:sqlserver://" + SERVER_NAME + ":1433;database=" + DATABASE_NAME;
+    private final String CONNECTION_URL = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s@sql-hit-omer;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30",SERVER_NAME,DATABASE_NAME,USERNAME, PASSWORD);
+
+    public AzureSqlConnection()
+    {
+        System.out.println(SERVER_NAME);
+        try (Connection connection = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD)) {
+            System.out.println("Connected to Azure SQL Database successfully!");
+
+            // Perform database operations here if needed
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     try (Connection connection = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD)) {
+    //         System.out.println("Connected to Azure SQL Database successfully!");
+
+    //         // Perform database operations here if needed
+
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+}
---
+++ src/Store/PropertiesReader.java
@@ -0,0 +1,23 @@
+package Store;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesReader {
+    private Properties properties;
+
+    public PropertiesReader(String filePath) {
+        properties = new Properties();
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
