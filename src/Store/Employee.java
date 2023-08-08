cb97667305ee4d1b10f73e430c58ffd144a746ea
Omer-5 <OmerHazan5@gmail.com>
2023-08-08 17:04:42 +0300

    database create table query

--- src/Store/Employee.java
+++ src/Store/Employee.java
@@ -3,7 +3,22 @@ package Store;
 public abstract class Employee extends Person {
    int bankAccount;
    int branch;
-   int employeeID; 
+   int employeeNumber; 
    String password;
    EmployeeTitle title;
 }
+
+
+/*
+ CREATE TABLE Employees (
+    FullName nvarchar(255) NOT NULL,
+	PhoneNumber nvarchar(255) NOT NULL,
+    ID int NOT NULL,
+    BankAccount int NOT NULL,
+	Branch int NOT NULL,
+	EmployeeNumber int IDENTITY(1,1) NOT NULL,
+	Password nvarchar(255) NOT NULL,
+	EmployeeTitle int NOT NULL,
+    PRIMARY KEY (ID)
+);
+ */
