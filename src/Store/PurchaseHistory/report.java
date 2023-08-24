348f99b5202daa5ed1fc261a65ba0f011fb6299a
Omer-5 <OmerHazan5@gmail.com>
2023-08-24 20:12:09 +0300

    basic structure

---
+++ src/Store/PurchaseHistory/report.java
@@ -0,0 +1,14 @@
+package Store.PurchaseHistory;
+
+import java.util.List;
+
+public class report {
+    private List<PurchasedItem> history;
+
+    
+    // getReportByCategory
+    // getReportByBranch
+    // getReportByProductID
+
+    //TODO: Export report to file
+}
