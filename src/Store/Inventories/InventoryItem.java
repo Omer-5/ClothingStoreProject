8bcdd5ba34af21c21cff5210b0d164b16a224817
Omer-5 <OmerHazan5@gmail.com>
2023-08-24 17:51:50 +0300

    private

--- src/Store/Inventories/InventoryItem.java
+++ src/Store/Inventories/InventoryItem.java
@@ -1,11 +1,12 @@
 package Store.Inventories;
 
 public class InventoryItem {
-    String branch;
-    int productID;
-    String name;
-    int quantity;
-    double price;
+    private String branch;
+    private int productID;
+    private String name;
+    private String category;
+    private int quantity;
+    private double price;
 }
 
 /*
@@ -13,6 +14,7 @@ CREATE TABLE Inventory (
     branch nvarchar(255) NOT NULL,
     productID int IDENTITY(1,1) NOT NULL,
 	name nvarchar(255) NOT NULL,
+    category nvarchar(255) NOT NULL,
     quantity int NOT NULL,
     price float NOT NULL,
     PRIMARY KEY (productID)
