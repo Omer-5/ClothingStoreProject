115679ef18efee2123ef1139c984b71e7886ec70
Omer-5 <OmerHazan5@gmail.com>
2023-08-24 20:05:01 +0300

    structure change

--- src/Store/PurchaseHistory/PurchaseHistory.java
+++ src/Store/PurchaseHistory/PurchaseHistory.java
@@ -1,22 +1,14 @@
 package Store.PurchaseHistory;
 
+import Store.Inventories.InventoryItem;
+
 public class PurchaseHistory {
     private int purchaseID;
-    private String branch;
-    private int productID;
-    private String name;
-    private String category;
-    private int quantity;
-    private double price;
+    private InventoryItem item;
 
-    public PurchaseHistory(int purchaseID, String branch, int productID, String name, String category, int quantity, double price) {
+    public PurchaseHistory(int purchaseID, InventoryItem item) {
         this.purchaseID = purchaseID;
-        this.branch = branch;
-        this.productID = productID;
-        this.name = name;
-        this.category = category;
-        this.quantity = quantity;
-        this.price = price;
+        this.item = item;
     }
 
     public int getPurchaseID() {
@@ -27,64 +19,19 @@ public class PurchaseHistory {
         this.purchaseID = purchaseID;
     }
 
-    public String getBranch() {
-        return branch;
+    public InventoryItem getItem() {
+        return item;
     }
 
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
-
-    public int getProductID() {
-        return productID;
-    }
-
-    public void setProductID(int productID) {
-        this.productID = productID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+    public void setItem(InventoryItem item) {
+        this.item = item;
     }
 }
 
 /*
-CREATE TABLE PurchaseHistory (
+CREATE TABLE PurchaseHistory(
     purchaseID int IDENTITY(1,1) NOT NULL,
-    branch nvarchar(255) NOT NULL,
-    productID int IDENTITY(1,1) NOT NULL,
-	name nvarchar(255) NOT NULL,
-    category nvarchar(255) NOT NULL,
-    quantity int NOT NULL,
-    price float NOT NULL,
-    PRIMARY KEY (productID)
+    productID int NOT NULL,
+    PRIMARY KEY (purchaseID)
 );
  */
