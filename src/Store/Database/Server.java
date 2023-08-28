87b983339730b90dadff6a7ae981df37cf0a350d
Omer-5 <OmerHazan5@gmail.com>
2023-08-28 20:40:52 +0300

    try-catch

--- src/Store/Database/Server.java
+++ src/Store/Database/Server.java
@@ -8,23 +8,27 @@ import java.util.Vector;
 
 public class Server {
     public static void main(String[] args)  throws IOException{
-        final ServerSocket server = new ServerSocket(7000);
-        System.out.println(new Date() + " --> Server waits for clients ....");
+        try (ServerSocket server = new ServerSocket(7000)) {
+            System.out.println(new Date() + " --> Server waits for clients ....");
 
-        while(true)
-        {
-            final Socket socket = server.accept();
-            new Thread(new Runnable() {
-                @Override
-                public void run()
-                {
-                    SocketData currentSocketData = new SocketData(socket);
-                    System.out.println(new Date() + " --> Client connected from "+ currentSocketData.getClientAddress());
+            while(true)
+            {
+                final Socket socket = server.accept();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        SocketData currentSocketData = new SocketData(socket);
+                        System.out.println(new Date() + " --> Client connected from "+ currentSocketData.getClientAddress());
 
-                    // Connected - Main Logic
-                    
-                }
-            }).start();
+                        // Connected - Main Logic
+                        
+                    }
+                }).start();
+            }
+        } catch ( IOException e)
+        { // Can't Create server
+            e.printStackTrace();
         }
     }
 }
