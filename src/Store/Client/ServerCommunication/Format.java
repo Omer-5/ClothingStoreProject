f7019d5fce2cd071426119370c1ec269316b38bc
Omer-5 <OmerHazan5@gmail.com>
2023-09-04 20:51:03 +0300

    wip

--- src/Store/Client/ServerCommunication/Format.java
+++ src/Store/Client/ServerCommunication/Format.java
@@ -5,18 +5,31 @@ public class Format {
     String typeSeparator = "@#@";
     String methodSeparator = "$%$";
     String paramsSeparator = "&*&";
+    static String fieldSeparator = "**";
 
     public Format(ClassType type, String methodName, String param1, String param2){
 
     }
 
-    public ClassType getType()
+    public ClassType getType(String str)
     {
-
+        String typeStr = str.split(typeSeparator)[0];
+        ClassType result = null;
+        switch (typeStr) {
+            case "Customer":
+                result = ClassType.CUSTOMER;
+                break;
+        
+            }
+            return result;
     }
 
-    public String getMethod()
+    public String getMethod(String str)
     {
+        String temp = str.split(this.methodSeparator)[0];
+        String methodStr = str.split(typeSeparator)[1];
+  
+        return methodStr;
 
     }
     
