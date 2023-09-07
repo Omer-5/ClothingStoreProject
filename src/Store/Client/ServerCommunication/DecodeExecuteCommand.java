b87647e33d621f2545eb9a00f741c85c2daba9a4
Omer-5 <OmerHazan5@gmail.com>
2023-09-07 23:48:52 +0300

    add more structure

--- src/Store/Client/ServerCommunication/DecodeExecuteCommand.java
+++ src/Store/Client/ServerCommunication/DecodeExecuteCommand.java
@@ -4,16 +4,26 @@ import Store.Client.ServerCommunication.Format;
 import Store.Client.ServerCommunication.DecodeExecuteCommandCustomer;
 
 public class DecodeExecuteCommand {
-    // TODO return type??
-    public static void decode_and_execute(String command)
+    public static String decode_and_execute(String command)
     {
+        String response = "";
         switch (Format.getType(command)) {
+            case CHAT:
+                break;
             case CUSTOMER:
-                DecodeExecuteCommandCustomer.execute(command);              
-            break;
-            
+                response = DecodeExecuteCommandCustomer.execute(command);
+                break;
+            case EMPLOYEE:
+                break;
+            case EXCEPTION:
+                break;
+            case INVENTORY:
+                break;
+            case PURCHASE_HISTORY:
+                break;
             default:
-            break;
+                break;
         }
+        return response;
      }
 }
