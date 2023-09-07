a88e2add973adb616211a2ae93e3b7f083d18f77
Omer-5 <OmerHazan5@gmail.com>
2023-09-07 20:47:52 +0300

    working `List` encode&decode for customer

--- src/Store/Client/ServerCommunication/Format.java
+++ src/Store/Client/ServerCommunication/Format.java
@@ -1,11 +1,19 @@
 package Store.Client.ServerCommunication;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Store.Customers.Customer;
+import Store.Employees.Employee;
+
 // [TYPE] | [METHOD_NAME] | [PARAMETERS]
 public class Format {
+    //DON'T USE `%` format
     public static String typeSeparator = "@@@";
     public static String methodSeparator = "###";
     public static String paramsSeparator = "&&&";
-    public static String fieldSeparator = "**";
+    public static String objectSeparator = "~~~";
+    public static String fieldSeparator = "!!!";
 
     public static String encode(ClassType type, String methodName, String param1, String param2){
         return encode(type, methodName, param1) + param2 + paramsSeparator;
@@ -46,5 +54,27 @@ public class Format {
         String temp = str.split(paramsSeparator)[1];
         return temp.split(paramsSeparator)[0];
     }
+
+   public static List<Customer> decodeCustomers(String str)
+   {
+       String[] objects = str.split(objectSeparator);
+       List<Customer> arr = new ArrayList<>();
     
+       for(String objectString: objects){
+           arr.add(Customer.deserializeFromString(objectString));
+       }
+       return arr;
+   } 
+
+   public static String encodeCustomers(List<Customer> arr)
+   {
+        StringBuilder result = new StringBuilder();
+        System.out.println(arr);
+        for(Customer customer: arr){
+            result.append(customer.serializeToString());
+            result.append(objectSeparator);
+        }
+        System.out.println(result);
+        return result.toString();
+   } 
 }
